53f5a64f3edc66855cee496bba71584ac7b332a5
helepires <a.pireshelena@gmail.com>
2026-03-18 19:24:21 -0300

    changing decimals

--- fluid_flow_rate.py
+++ fluid_flow_rate.py
@@ -4,4 +4,4 @@ tube_radius = tube_diameter_input / 2
 tube_area_section = math.pi * (tube_radius**2)
 fluid_velocity = float(input("Digite a velocidade do fluido (m/s): "))
 flow_rate = tube_area_section * fluid_velocity
-print(f"A vazão do tubo é de: {flow_rate:.2f}")
+print("A vazão do tubo é de: %.2f metros cúbicos por segundo" % flow_rate)
